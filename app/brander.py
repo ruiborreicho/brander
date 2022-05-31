91ae9579d55386469d102aa2710602a810adec9b
Rui Borreicho <180100436@esg.ipsantarem.pt>
2022-06-01 00:31:12 +0100

    first complete version of brander.py

--- app/brander.py
+++ app/brander.py
@@ -4,26 +4,30 @@ import argparse
 import re
 from typing import List
 
-def main():
-    print("Running Brander")
+MAX_INPUT_LENGTH = 32
 
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
     user_input = args.input
 
     print(f"User Input: {user_input}")
-    branding_results = generate_branding_snippet(user_input)
-    keywords_results = generate_keywords(user_input)
+    if validate_length(user_input):
+        branding_results = generate_branding_snippet(user_input)
+        keywords_results = generate_keywords(user_input)
+    else:
+        raise ValueError(f"Input length is too long. Must be under {MAX_INPUT_LENGTH}. Submitted input is {user_input}")
 
-    print(branding_results)
-    print(keywords_results)
+def validate_length(prompt: str) -> bool:
+    return len(prompt) <= MAX_INPUT_LENGTH
 
 def generate_branding_snippet(prompt: str) -> str:
     #Load the API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     enriched_prompt=f"Generate upbeat branding snippet for {prompt}: "
+    print(enriched_prompt)
 
     response = openai.Completion.create(engine="text-davinci-001", prompt=enriched_prompt, max_tokens=32)
 
@@ -39,6 +43,7 @@ def generate_branding_snippet(prompt: str) -> str:
     if last_char not in {".", "!", "?"}:
         branding_text += "..."
 
+    print(f"Snippet: {branding_text}")
     return branding_text
 
 def generate_keywords(prompt: str) -> List[str]:
@@ -46,6 +51,7 @@ def generate_keywords(prompt: str) -> List[str]:
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     enriched_prompt=f"Generate related branding keywords for {prompt}: "
+    print(enriched_prompt)
 
     response = openai.Completion.create(engine="text-davinci-001", prompt=enriched_prompt, max_tokens=32)
 
@@ -58,6 +64,7 @@ def generate_keywords(prompt: str) -> List[str]:
     keywords_array = [k.lower().strip() for k in keywords_array]
     keywords_array = [k for k in keywords_array if len(k) > 0]
 
+    print(f"Keywords: {keywords_array}")
     return keywords_array
 
 if __name__ == "__main__":
